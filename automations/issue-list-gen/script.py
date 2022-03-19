60eadf0dbdb0c0f712f796a768f8dd39c304bf78
z9fr <z9fr@protonmail.com>
2022-03-19 22:54:43 +0530

    getting all the open issues of a repo

--- automations/issue-list-gen/script.py
+++ automations/issue-list-gen/script.py
@@ -1,7 +1,24 @@
 #/usr/bin/python3
+from github import Github
+from dotenv import dotenv_values
+
 
 def main():
+    print("main")
     # call here
 
 def requestDetails():
+    config = dotenv_values(".env") 
+    gh = Github(config['token'])
+
+    repo = gh.get_repo("Satriaana/DevOps")
+    open_issues = repo.get_issues(state='open')
+
+    for issue in open_issues:
+        print(issue)
+
+
+requestDetails()
+
+
 
