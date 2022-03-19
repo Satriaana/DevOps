f52a3f4245ce5336be36845035567f26808ba8ed
z9fr <z9fr@protonmail.com>
2022-03-20 00:18:57 +0530

    loop though the repos and getting relevent information

--- automations/issue-list-gen/script.py
+++ automations/issue-list-gen/script.py
@@ -1,22 +1,39 @@
 #/usr/bin/python3
 from github import Github
 from dotenv import dotenv_values
+import yaml
+
+config = dotenv_values(".env") 
 
 
 def main():
-    print("main")
+    readYaml("./example.yml")
+    #requestDetails("devOps")
     # call here
 
-def requestDetails():
-    config = dotenv_values(".env") 
-    gh = Github(config['token'])
-
-    repo = gh.get_repo("Satriaana/DevOps")
+def requestDetails(repo):
+    gh = Github(config['GH_TOKEN'])
+    repo = gh.get_repo(f"Satriaana/{repo}")
     open_issues = repo.get_issues(state='open')
 
     for issue in open_issues:
         print(issue)
 
+
+def readYaml(yamlfile):
+    with open(yamlfile) as file:
+        data = yaml.safe_load(file)
+    Projects = data['Satriaana']['Projects']
+
+    for i in Projects:
+        repos = Projects[i]['Repo-list']
+
+        if not repos:
+            print("nah")
+        else:
+            for repo in repos:
+                print(repo)
+                requestDetails(repo)
 
 if __name__ == "__main__":
     main()
